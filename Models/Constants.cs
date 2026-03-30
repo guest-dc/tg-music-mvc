7e82b2e7fd9f36a1af8cfcb0ce12e3c32a37c2c7
Davis Guest <davisdevice@gmail.com>
2026-03-30 00:09:52 -0400

    init layout & header, added bs-icons

---
+++ Models/Constants.cs
@@ -0,0 +1,17 @@
+public class Constants
+{
+   public static class SocialLinks
+   {
+      public const string Instagram = "https://www.instagram.com/thomasguestmusic/";
+      public const string Facebook = "https://www.facebook.com/people/Thomas-Guest/61553548274388/";
+      public const string TikTok = "https://www.tiktok.com/@officialthomasguest";
+      public const string Spotify = "https://open.spotify.com/artist/2TMi9BlE8IlHh43hlEdfRM?si=ymUD8UsjT5-qhJQqyq4L-g";
+      public const string YouTube = "https://www.youtube.com/@ThomasGuestMusic";
+      public const string YouTubeMusic = "https://music.youtube.com/channel/UCODHo8_ZBj75kquwKXzN8pg";
+   }
+
+   public static class Emails
+   {
+      public const string BookingEmail= "thomasguestbookings@gmail.com";
+   }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -25,5 +22,12 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
-app.Run($"http://0.0.0.0:{port}");
+if(app.Environment.IsDevelopment())
+{
+    app.Run();
+}
+else
+{
+    var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+    app.Run($"http://0.0.0.0:{port}");
+}
